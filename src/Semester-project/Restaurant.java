3e5af30dda8557089e3fcb32fa56c01a517d6c0c
Nicholas "LB" Braden <LB@LB-Stuff.com>
2013-11-13 22:46:38 -0600

    Begin semester project

---
+++ src/Semester-project/Restaurant.java
@@ -0,0 +1,15 @@
+/**
+ * CS2336 Semester Project
+ * by Raul & Nicholas
+ * rig130030
+ * ngb130030
+ */
+
+public class Restaurant
+{
+	public static void main(String[] args)
+	{
+		//...
+	}
+}
+ 
