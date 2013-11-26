6f52fe79f6e12e8d05bbd8a582935aec059c4a04
Raul1001 <rig130030@utdallas.edu>
2013-11-26 15:20:45 -0600

    Table class fix

--- src/SemesterProject/Table.java
+++ src/SemesterProject/Table.java
@@ -1,29 +1,29 @@
 package SemesterProject;
 
 public class Table {
-	
+
 	private String label;
 	private char status;
-	
-	public Table(String label){
-		
+
+	public Table(String label) {
+
 	}
-	
-	public void setLabel(String label){
-		label = label;
+
+	public void setLabel(String label) {
+		this.label = label;
 	}
-	
-	public void setStatus(String s){
-		//..
+
+	public void setStatus(String s) {
+		// ..
 	}
-	
-	public String getLabel(){
+
+	public String getLabel() {
 		String label = null;
 		return label;
 	}
-	
-	public char getStatus(){
+
+	public char getStatus() {
 		return status;
 	}
-	
+
 }
